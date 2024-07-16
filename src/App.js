2629cae7da7359eaadf625e2cbe63491e4220e4f
alaaAbdallah <alaa_abd_1999@hotmail.com>
2024-07-16 23:27:35 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
+import Home from './Home.js';
+
+
+export default function App() {
+  return (
+    <>
+  <Home/>
+  <Footer/>
+  <Navbar/>
+  </>
+  );
+}
+
---
+++ src/Footer.js
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+  return (
+    <div className="alert alert-danger">Footer</div>
+  )
+}
